310dc56f8e3133eddcd2622c4665dfd0920bcd39
Marian Dan <64864410+mariandanGit@users.noreply.github.com>
2021-04-12 20:12:28 +0300

    Update contactForm.php

--- contactForm.php
+++ contactForm.php
@@ -6,13 +6,19 @@ if (isset($_POST['submit'])) {
     $mailFrom=$_POST['email'];
     $message=$_POST['message'];    
 
-    $mailTo = "danmarian915@yahoo.com";
-    $header = "From: ".$mailFrom;
-    $txt = "You have recieved a mail from ".$name.".\n\n".$message;
-    $subject = "Personal Portfolio Mail";
+    if (($name=="")||($mailFrom=="")||($message=="")){
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+    }
+    else{
+        
+        $mailTo = "danmarian915@yahoo.com";
+        $header = "From: ".$mailFrom;
+        $txt = "You have recieved a mail from ".$name.".\n\n".$message;
+        $subject = "Personal Portfolio Mail";
 
-    mail($mailTo, $subject, $txt, $header);
+        mail($mailTo, $subject, $txt, $header);
    
-    echo "Message sent!";
+        echo "Message sent!";
+    }
 }  
 ?>
